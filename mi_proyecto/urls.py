05c37449e44455ea6bb59221ed1b7920601f99dc
genesismayuri <mayuriremache0@gmail.com>
2026-03-12 10:13:36 -0500

    Estilos mejorados

--- mi_proyecto/urls.py
+++ mi_proyecto/urls.py
@@ -9,6 +9,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,4 +24,4 @@ urlpatterns = [
 # Servir archivos estáticos en desarrollo
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += staticfiles_urlpatterns()
